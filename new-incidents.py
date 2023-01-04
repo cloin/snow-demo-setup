9a81cac6a89d3d4d9ae23b223444f9479de9721f
Colin McNaughton <cloin@users.noreply.github.com>
2023-01-04 15:10:03 -0500

    Create new-incidents.py

---
+++ new-incidents.py
@@ -0,0 +1,44 @@
+import asyncio
+import aiohttp
+import json
+import os
+import time
+
+# Poll ServiceNow API for new incidents and print out the incident number, sys_id, incident_status and short description as a JSON object.
+# Uses asyncio and aiohttp, and authenticates using aiohttp.BasicAuth with the login credentials and instance URL provided as environment variables.
+# Only prints out incidents created after the script began executing, and only prints out each incident once.
+
+async def check_incidents(start_time, interval, printed_incidents):
+    username = os.environ['SN_USERNAME']
+    password = os.environ['SN_PASSWORD']
+    instance = os.environ['SN_HOST']
+
+    async with aiohttp.ClientSession() as session:
+        auth = aiohttp.BasicAuth(login=username, password=password)
+        while True:
+            async with session.get(f'{instance}/api/now/table/incident?sysparm_query=sys_created_onONToday@javascript:gs.beginningOfToday()@javascript:gs.endOfToday()', auth=auth) as resp:
+                if resp.status == 200:
+                    incidents = await resp.json()
+                    for incident in incidents['result']:
+                        if incident['sys_updated_on'] > start_time and incident['sys_id'] not in printed_incidents:
+                            printed_incidents.add(incident['sys_id'])
+                            print(json.dumps({
+                                'number': incident['number'],
+                                'sys_id': incident['sys_id'],
+                                'short_description': incident['short_description'],
+                                'incident_state': incident['incident_state'],
+                            }))
+                else:
+                    print(f'Error {resp.status}')
+            await asyncio.sleep(interval)  # Poll every interval seconds
+
+async def main():
+    start_time = time.time()
+    start_time_str = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(start_time))
+    interval = 1 # Poll every 1 seconds
+    printed_incidents = set()
+    await check_incidents(start_time_str, interval, printed_incidents)
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
