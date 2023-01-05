6bdb0ed91314a149e155b7863181eff5ede1d05a
Colin McNaughton <cloin@users.noreply.github.com>
2023-01-05 12:44:38 -0500

    Update new-incidents.py

--- new-incidents.py
+++ new-incidents.py
@@ -12,18 +12,21 @@ import time
 #  {"number": "INC0012170", "sys_id": "b7206ff71b88e950b76a0d0fdc4bcb7b", "short_description": "test incident 1", "incident_state": "1"}
 #  {"number": "INC0012171", "sys_id": "4630eb7b1b88e950b76a0d0fdc4bcb4e", "short_description": "test incident 2", "incident_state": "1"}
 
-async def check_incidents(start_time, interval, printed_incidents):
+async def check_incidents(start_time, printed_incidents):
     username = os.environ['SN_USERNAME']
     password = os.environ['SN_PASSWORD']
     instance = os.environ['SN_HOST']
+    interval = 1 # Poll every 1 seconds
 
     async with aiohttp.ClientSession() as session:
         auth = aiohttp.BasicAuth(login=username, password=password)
         while True:
             async with session.get(f'{instance}/api/now/table/incident?sysparm_query=sys_created_onONToday@javascript:gs.beginningOfToday()@javascript:gs.endOfToday()', auth=auth) as resp:
                 if resp.status == 200:
+
                     incidents = await resp.json()
                     for incident in incidents['result']:
+
                         if incident['sys_updated_on'] > start_time and incident['sys_id'] not in printed_incidents:
                             printed_incidents.add(incident['sys_id'])
                             print(json.dumps({
@@ -39,9 +42,8 @@ async def check_incidents(start_time, interval, printed_incidents):
 async def main():
     start_time = time.time()
     start_time_str = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(start_time))
-    interval = 1 # Poll every 1 seconds
     printed_incidents = set()
-    await check_incidents(start_time_str, interval, printed_incidents)
+    await check_incidents(start_time_str, printed_incidents)
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
