a93e36bba7c34ef49ad4c9495e3f4c31e449305f
colin <colin@redhat.com>
2023-01-25 19:47:45 -0500

    devcontainer install ansible reqs

---
+++ eda/install_reqs.sh
@@ -0,0 +1,4 @@
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+pip install -r $SCRIPTPATH/requirements.txt
+ansible-galaxy collection install -r $SCRIPTPATH/requirements.yml
+ansible-playbook -i localhost, -c local ansible.eda.install_rulebook_cli
