a20c66d783041e39ef7d967ec503f25089990fc9
colin <colin@redhat.com>
2023-01-25 21:32:47 -0500

    install deps on postcreate

--- .devcontainer/install_reqs.sh
+++ .devcontainer/install_reqs.sh
@@ -1,4 +1,8 @@
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-pip install -r $SCRIPTPATH/../requirements.txt
-ansible-galaxy collection install -r $SCRIPTPATH/../requirements.yml
+# install python dependencies
+pip install -r $1/requirements.txt
+
+# install ansible collections
+ansible-galaxy collection install -r $1/requirements.yml
+
+# install ansible-rulebook (https://github.com/ansible/event-driven-ansible#getting-started)
 ansible-playbook -i localhost, -c local ansible.eda.install_rulebook_cli
